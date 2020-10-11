b73d37b394756e8340da1d404f8efae8287f079d
cdavis0 <cdavis0>
2020-10-11 17:14:42 -0500

    Oct 11 20

---
+++ USBDeviceTutorial/src/Ethernet_entry.c
@@ -0,0 +1,11 @@
+#include "Ethernet.h"
+
+/* Ethernet entry function */
+void Ethernet_entry(void)
+{
+    /* TODO: add your own code here */
+    while (1)
+    {
+        tx_thread_sleep (1);
+    }
+}
---
+++ USBDeviceTutorial/src/USB_entry.c
@@ -0,0 +1,12 @@
+#include "USB.h"
+#include <Ganymede.h>
+
+/* USB entry function */
+void USB_entry(void)
+{
+    /* TODO: add your own code here */
+    while (1)
+    {
+        tx_thread_sleep (1);
+    }
+}
